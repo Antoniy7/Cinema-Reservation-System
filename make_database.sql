cd0a163047b30e16b18a5734060aa2af71fce40b
Antoniy7 <antoniy_pavlov@abv.bg>
2015-05-07 21:22:30 +0300

    deiba

---
+++ make_database.sql
@@ -0,0 +1,32 @@
+PRAGMA foreign_keys=ON;
+
+DROP TABLE IF EXISTS Movies;
+
+CREATE TABLE Movies(
+  movie_id INTEGER PRIMARY KEY,
+  movie_name TEXT,
+  movie_rating FLOAT
+);
+
+DROP TABLE IF EXISTS Projections;
+
+CREATE TABLE Projections(
+  projection_id INTEGER PRIMARY KEY,
+  projection_movie_id INTEGER,
+  FOREIGN KEY(projection_movie_id) REFERENCES Movies(movie_id),
+  projection_type TEXT,
+  projeciton_date DATE,
+  projection_time INTEGER
+);
+
+DROP TABLE IF EXISTS Reservations;
+
+CREATE TABLE Reservations(
+  reservation_id INTEGER PRIMARY KEY,
+  user_name TEXT,
+  row INTEGER,
+  col INTEGER,
+  reservation_projection_id INTEGER,
+  FOREIGN KEY(reservation_projection_id) REFERENCES Projections(projection_id)
+);
+
