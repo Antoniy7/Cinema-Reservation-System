7688b3259b131091393bac717ad6e6cbee40267b
Antoniy7 <antoniy_pavlov@abv.bg>
2015-05-08 19:17:42 +0300

    rename

---
+++ magic_reservation_system.py
@@ -0,0 +1,42 @@
+#conn = sqlite3.connect(DB_NAME)
+#conn.row_factory = sqlite3.Row
+#deiba
+
+class cinema_database_enquery:
+
+    def __init__(self, conn):
+        self.conn = conn
+  
+    def get_all_movies(self):
+        cursor = self.conn.cursor()
+        result = cursor.execute("""
+        SELECT movie_id, movie_name, movie_rating
+        FROM Movies ORDER BY movie_rating
+        """ )
+        return result.fetchall()
+
+    def get_all_movies_by_id_and_date(self, movie_id, movie_date):
+        cursor = self.conn.cursor()
+        result = cursor.execute("""
+            SELECT a.name, b.data, b.time, b.type
+            FROM projections as b 
+            JOIN movies as a
+            ON a.id = b.movie_id
+            WHERE b.movie_id 
+            """)
+        return result.fetchall()
+
+    def get_all_movies_by_id(self, movie_id):
+        cursor = self.conn.cursor()
+        result = cursor.execute(query)
+        return result.fetchall()
+
+    def get_num_of_free_seats_by_movie_id(self, movie_id):
+        cursor = self.__conn.cursor()
+        result = cursor.execute(query)
+        return result.fetchall()
+
+    def get_num_of_seats_by_projection_id(self, projection_id):
+        cursor = self.__conn.cursor()
+        result = cursor.execute(query)
+        
